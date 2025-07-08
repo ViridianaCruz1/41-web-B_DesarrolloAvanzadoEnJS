68d29a72c912f35ec42f6674473488a261da3dc9
ViridianaCruz1 <viridianacruz2010@gmail.com>
2025-07-07 17:08:03 -0700

    Add files via upload

---
+++ Proyecto_Intro_a_NodeJS_y_npm/mi-exploracion-espacial/planetas.js
@@ -0,0 +1,35 @@
+const planetas = [
+    {
+        nombre: "Titán",
+        descripcion: "La luna más grande de Saturno, con lagos de metano.",
+        descubiertoEn: "1655"
+    },
+    {
+        nombre: "Próxima Centauri b",
+        descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+        descubiertoEn: "2016"
+    },
+    {
+        nombre: "Marte",
+        descripcion: "Planeta rocoso del Sistema Solar, conocido por su color rojizo y considerado uno de los cuerpos celestes más explorados por sondas y robots.",
+        descubiertoEn: "Antigüedad (conocido desde tiempos prehistóricos)"
+    },
+    {
+        nombre: "Neptuno",
+        descripcion: "Gigante gaseoso azul del Sistema Solar, descubierto gracias a predicciones matemáticas debido a irregularidades en la órbita de Urano.",
+        descubiertoEn: "1846"
+    },
+    {
+        nombre: "Kepler-22b",
+        descripcion: "Exoplaneta situado en la zona habitable de su estrella, a unos 600 años luz de la Tierra, con posibilidad de albergar agua líquida.",
+        descubiertoEn: "2011"
+    },
+    {
+        nombre: "HD 209458 b",
+        descripcion: "Primer exoplaneta observado en tránsito frente a su estrella; es un Júpiter caliente con atmósfera gaseosa.",
+        descubiertoEn: "1999"
+    },
+  // ¡Añade más planetas!
+];
+
+module.exports = planetas;
